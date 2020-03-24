b3d0434a4c04d681280e6fcc81f88b3e7fc031a0
Neville Dabre <nevil_dabre@yahoo.com>
2020-03-24 01:19:12 -0400

    sticky header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
-import "./styles.css";
+import React from "react";
 import LandingPage from "./components/LandingPage/LandingPage";
-import LearningPage from "./components/LearningPage/LearningPage";
 import LearningPageLayout from "./components/LearningPage/LearningPageLayout";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
-  const [textSize, setTextSize] = useState(0);
-
-  const handleTextSizeChanged = type => {
-    switch (type) {
-      case "increase":
-        setTextSize(textSize + 0.2);
-        break;
-      case "decrease":
-        setTextSize(textSize - 0.2);
-        break;
-      default:
-        null;
-    }
-  };
   return (
     <div className="App">
       <Router>
@@ -28,10 +12,6 @@ export default function App() {
           <Route path="/contact" />
           <Route path="/learningpage">
             <LearningPageLayout />
-            {/* <LearningPage
-              textSize={textSize}
-              handleTextSizeChanged={handleTextSizeChanged}
-            /> */}
           </Route>
         </Switch>
       </Router>
--- src/components/LandingPage/Header/Header.js
+++ src/components/LandingPage/Header/Header.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [headerActive, setHeaderActive] = useState(false);
+  const handleScroll = () => {
+    console.log("Here");
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > 10) {
+      setHeaderActive(true);
+    } else {
+      setHeaderActive(false);
+    }
+  };
+
+  useEffect(() => {}, [headerActive]);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header>
+    <header className={headerActive ? "active" : ""}>
       <h2>
         <a href="https://google.ca">D2L</a>
       </h2>
